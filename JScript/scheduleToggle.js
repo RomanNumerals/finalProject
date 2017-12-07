ad623bf62f100908d829016d3f4fc97d28153af9
Brandon <31549535+Bgreen23@users.noreply.github.com>
2017-12-07 16:09:36 -0500

    Got the colors on the schedule page to show the current view

--- JScript/scheduleToggle.js
+++ JScript/scheduleToggle.js
@@ -1,14 +1,19 @@
 $(document).ready(() => {
     $('.calendarView').show();
     $('.listView').hide();
+    $('.calendarToggle').css('color', '#5bd8c8');
 
     $('.listToggle').on('click',()=> {
       $('.listView').show('slow','linear');
+      $('.listToggle').css('color', '#5bd8c8');
+      $('.calendarToggle').css('color', 'black');
       $('.calendarView').hide();
     });
 
     $('.calendarToggle').on('click', () => {
       $('.calendarView').show('fast');
-      $('.listView').hide('');
+      $('.calendarToggle').css('color', '#5bd8c8');
+      $('.listToggle').css('color', 'black');
+      $('.listView').hide();
     });
 });
