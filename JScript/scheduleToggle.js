4ce40fed603d5867c8efc481ebf80ee6b04e0cc2
Brandon <31549535+Bgreen23@users.noreply.github.com>
2017-12-08 15:39:05 -0500

    did some things

--- JScript/scheduleToggle.js
+++ JScript/scheduleToggle.js
@@ -4,14 +4,14 @@ $(document).ready(() => {
     $('.calendarToggle').css('color', '#5bd8c8');
 
     $('.listToggle').on('click',()=> {
-      $('.listView').show('slow','linear');
+      $('.listView').show('fast','linear');
       $('.listToggle').css('color', '#5bd8c8');
       $('.calendarToggle').css('color', 'black');
       $('.calendarView').hide();
     });
 
     $('.calendarToggle').on('click', () => {
-      $('.calendarView').show('fast');
+      $('.calendarView').show('slow');
       $('.calendarToggle').css('color', '#5bd8c8');
       $('.listToggle').css('color', 'black');
       $('.listView').hide();
