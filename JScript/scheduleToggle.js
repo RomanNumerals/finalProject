8fc17f7f589ab2bd6dc2385881814f7b50c6422a
Brandon <31549535+Bgreen23@users.noreply.github.com>
2017-12-13 13:31:42 -0500

    still a WIP but some critical fixes were made that need to be merged

--- JScript/scheduleToggle.js
+++ JScript/scheduleToggle.js
@@ -1,15 +1,17 @@
+//this is the toggle for the schedule page that changes the view of the schedule
 $(document).ready(() => {
+  //initial page view
     $('.calendarView').show();
     $('.listView').hide();
     $('.calendarToggle').css('color', '#5bd8c8');
-
+    //shows list view
     $('.listToggle').on('click',()=> {
       $('.listView').show('fast','linear');
       $('.listToggle').css('color', '#5bd8c8');
       $('.calendarToggle').css('color', 'black');
       $('.calendarView').hide();
     });
-
+    //shows calendar
     $('.calendarToggle').on('click', () => {
       $('.calendarView').show('slow');
       $('.calendarToggle').css('color', '#5bd8c8');
